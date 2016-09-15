330064ad7cd0ef2fc156a6291585b56b72177343
Austen McDonald <austenmc@fb.com>
2016-09-14 21:28:45 -0700

    It's alive\!

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# smf-sphinx2downloadsystem
+# Simple Machines Forum Sphinx Download Manager --> Download System Converter
+
+This was built for Talisman at http://dragonprime.net to help him convert his
+Simple Machines Forum version 1.1.21, complete with unsupported mod called "Sphinx Download Manager"
+originally built by the apparently defunct Dynamic Systems & Content Solution, to the
+latest version of SMF without losing his download section, converting them to the [Download System](http://custom.simplemachines.org/mods/index.php?mod=992).
+
+## Usage
+1. First, edit `transfer.php` to set the `$server`, `$dbname`, `$username` and `$password` settings. If your 
+SMF install uses a different DB prefix (the text at the beginning of each table name) then edit the old and new table names.
+
+1. Next, simply run `php transfer.php`.
+
+Of course, you should back up your database before you do run this script :)
+
---
+++ transfer.php
@@ -0,0 +1,145 @@
+<?php
+
+$servername = 'localhost';
+$dbname = 'smf';
+$username = 'admin';
+$password = 'mypass';
+
+$oldCategoryTable = 'smf_download_cats';
+$oldDownloadTable = 'smf_download_items';
+
+$newCategoryTable = 'smf_down_cat';
+$newDownloadTable = 'smf_down_file';
+
+function main()
+{
+    global $servername, $dbname, $username, $password;
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully $servername:$dbname\n"; 
+    } catch(PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+
+    $categories = readOldCategories($conn);
+    $downloads = readOldDownloads($conn);
+
+    $categoryMapping = writeNewCategories($conn, $categories, $downloads);
+
+    writeNewDownloads($conn, $downloads, $categoryMapping);
+}
+
+// Returns an associative array of oldCategoryId => array of table
+// columns and values.
+function readOldCategories($conn)
+{
+    global $oldCategoryTable;
+
+    $stmt = $conn->prepare("SELECT id, name, description, isSub, permission FROM $oldCategoryTable;");
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+
+    $mapping = [];
+
+    foreach ($result as $row) {
+        $mapping[$row['id']] = $row;
+    }
+    return $mapping;
+}
+
+// Returns a mapping of old category id to new category id.
+function writeNewCategories($conn, $categories, $downloads)
+{
+    global $newCategoryTable;
+
+    // Count the number of downloads in each category and add them to $totals.
+    $totals = [];
+    foreach ($downloads as $oldId => $d) {
+        $oldCategoryId = $d['cat'];
+        if (array_key_exists($oldCategoryId, $totals)) {
+            $count = $totals[$oldCategoryId];
+            $count += 1;
+            $totals[$oldCategoryId] = $count;
+        } else {
+            $totals[$oldCategoryId] = 1;
+        }
+    } 
+
+    // Progressively insert the categories, starting with the root categories,
+    // since you can't insert a subcategory before its parent.
+    $inserted = [];
+
+    $stmt = $conn->prepare("INSERT INTO $newCategoryTable (title, description, id_parent, total) VALUES(?, ?, ?, ?)");
+    do {;
+        foreach ($categories as $oldId => $c) {
+            $oldParentId = $c['isSub'];
+            if (array_key_exists($oldId, $inserted)) {
+                // Already inserted this one.
+                continue;
+            }
+            // If this is a root category or we've already inserted the parent,
+            // insert a new category row.
+            if ($oldParentId == 0 || array_key_exists($oldParentId, $inserted)) {
+                $newParentId = $oldParentId == 0 ? 0 : $inserted[$oldParentId];
+                $name = $c['name'];
+
+                $total = 0;
+                if (array_key_exists($oldId, $totals)) {
+                    $total = $totals[$oldId];
+                }
+
+                echo "Inserting category oldId={$oldId} oldParentId={$oldParentId} name={$name}...";
+                $stmt->execute([$name, $c['description'], $newParentId, $total]);
+                // build a mapping of old to new IDs.
+                $newId = $conn->lastInsertId();
+                $inserted[$oldId] = $newId;
+                echo "newId={$newId}\n";
+            }
+        }
+    } while (count($inserted) < count($categories));
+
+    // Root category is still category 0, but we dont have to add it.
+    $inserted[0] = 0;
+
+    return $inserted;
+}
+
+// Returns an associative array of oldDownloadId => array of table
+// columns and values.
+function readOldDownloads($conn)
+{
+    global $oldDownloadTable;
+
+    $stmt = $conn->prepare("SELECT id, cat, name, ddesc, filename, author_name, author_email, filesize, downloads, website, website_title, date, views FROM $oldDownloadTable");
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+
+    $mapping = [];
+
+    foreach ($result as $row) {
+        $mapping[$row['id']] = $row;
+    }
+    return $mapping;
+}
+
+function writeNewDownloads($conn, $downloads, $categoryMapping)
+{
+    global $newDownloadTable;
+
+    $stmt = $conn->prepare("INSERT INTO $newDownloadTable (date, title, description, views, totaldownloads, filesize, orginalfilename, filename, id_cat, approved) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, 1)");
+
+    foreach ($downloads as $oldId => $d) {
+        $name = $d['name'];
+        $oldCatId = $d['cat'];
+        $newCatId = $categoryMapping[$oldCatId];
+        echo "Inserting download oldId={$oldId} title={$name} oldCatId={$oldCatId}...";
+        $stmt->execute([$d['date'], $d['name'], $d['ddesc'], $d['views'], $d['downloads'], $d['filesize'], $d['filename'], $d['filename'], $newCatId]);
+        $newId = $conn->lastInsertId();
+        echo "newId={$newId} newCatId={$newCatId}\n";
+     }
+}
+
+main();
